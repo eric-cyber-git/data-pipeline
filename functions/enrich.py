e3fd5dd5e0ff53492155f2093fedb1e57e555f21
eric-cyber-git <publikaddress@pm.me>
2025-06-23 19:30:14 -0400

    Update enrich.py

--- functions/enrich.py
+++ functions/enrich.py
@@ -1,26 +1,25 @@
-from functions.sql_extract import fetch_new_logs
-from functions.utils import load_checkpoint, update_checkpoint
-from functions.enrich import enrich_logs
-from functions.send_logs import send_logs
-
-
-last_seen_id = load_checkpoint()
-SQL_Logs= fetch_new_logs(last_seen_id)
-
-if SQL_Logs:
-
-    # Enrich the data returned from SQL
-    enriched_logs = enrich_logs(SQL_Logs)
-
-    # Update config file
-    last_id = max(log["log_id"] for log in SQL_Logs)
-    update_checkpoint(last_id)
-
-    # Forward enriched data to Splunk 
-    send_logs(enriched_logs)
-else:
-    print("No new logs to process.")
+from functions.virus_total_api import VT_API_IP_Report
+from functions.sql_extract import fetch_ip_counts
+import time
 
 
 
+def enrich_logs(logs):
+    """Enriches SQL log records with IP frequency counts and VirusTotal intelligence."""
+    
+    ip_rows = fetch_ip_counts()
+    # Create a map object using dictionary comprehension. 
+    ip_hit_map = {row['ip_address']: row['hit_count'] for row in ip_rows}
 
+    enriched = []
+    for record in logs:
+        ip = record["ip_address"]
+        # Update the record with the ip count, if present. If not present, return 1 for count. 
+        record["ip_hit_count"] = ip_hit_map.get(ip, 1)
+        # Account for VT API Rate Limit of 4 requests per minute
+        record.update(VT_API_IP_Report(ip))        
+        time.sleep(15)
+        enriched.append(record)
+
+
+    return enriched
